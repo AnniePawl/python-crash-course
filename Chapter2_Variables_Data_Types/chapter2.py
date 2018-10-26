ece71c3317d297283cffd014c356ac50cb1d3694
Anna Pawl <annampawl@gmail.com>
2018-10-26 12:49:47 -0700

    first commit

---
+++ Chapter2_Variables_Data_Types/chapter2.py
@@ -0,0 +1,16 @@
+# Print Statement-- easiest way to get python program to communicate with you via terminalself.
+#Dont' forget! In Python3, print statement needs parenthesis
+print ("Hello World")
+
+#My First Varable!
+message = "Life is Good"
+print (message)
+
+#Variable Rules
+# -Can contain letters, numbers, underscore
+# -Cannot start with a number
+# -NO spaces, use underscore
+# -Names should be short and descriptive
+name = "Ada Lovelace"
+print (name.title())
+# .title() is a method that
