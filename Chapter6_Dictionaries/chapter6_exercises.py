d9e4847b4ea1bf665f7354d777760a05ae8c6f7b
Anna Pawl <annampawl@gmail.com>
2018-11-11 21:54:19 -0800

    chapter 6 exercises, nesting

--- Chapter6_Dictionaries/chapter6_exercises.py
+++ Chapter6_Dictionaries/chapter6_exercises.py
@@ -12,12 +12,10 @@ Rebi = {
 'hair_color':'dirty blonde',
 'hair_length':'long',
 }
-
 Nicky = {
 'age': '25',
 'height':'5ft 4in',
 }
-
 print("My friend, Rebi is " + Rebi['age'] + " years old with " + Rebi['hair_length'] + " , " + Rebi['hair_color'] + " hair.")
 # Add a new key-value pair
 Rebi['favorite_color'] = 'teal'
@@ -34,7 +32,6 @@ print(favorite_numbers)
 # Use a loop to print something about each state
 # Use a loop to print each state (key)
 # Use loop to print each capital (value)
-
 state_capitals = {'alabama':'montgomery','arizona':'phoenix','california':'sacramento'}
 
 for state, capital in state_capitals.items():
@@ -46,6 +43,7 @@ for key, value in state_capitals.items():
 
 # Start with an empty dictionary
 # Add key-value pairs to it
+# Print keys and values
 
 pets = {}
 pets['anna'] = 'dog'
@@ -62,3 +60,37 @@ for person, pet in pets.items():
 
 for person, pet in pets.items():
     print(person.title() + " has a pet " + pet + ".")
+
+# Store a dictionary in dictionary
+fruit_description = {
+'banana':{'shape':'tubular','color':'yellow'},
+'apple':{'shape':'round','color':'red'},
+'pear':{'shape':'curvy','color':'green'},
+}
+# Make every fruit rainbow colored
+for info in fruit_description.values():
+    info['color'] = 'rainbow'
+    print(info)
+
+# Make several dictionaries, store them in a list
+# Include descriptions about that animal
+# Loop through and print everything you know about the animal
+animal1 = {'type':'elephant','color':'grey', 'texture':'fuzzy'}
+animal2 = {'type':'tiger','color':'black & orange', 'texture':'furry'}
+animal3 = {'type':'bear','color':'brown','texture':'fuzzy'}
+animals = [animal1, animal2, animal3]
+
+for animal in animals:
+    print(animal['type'].title() + "s are " + animal['color'].title() +  " and " + animal['texture'].title() + ".")
+
+# Dictionary of fave places
+# Loop through and print every person's fave destination
+fave_places = {
+'anna':'berlin',
+'jeremy':'tokyo',
+'donny':'kiev'
+}
+for key, value in fave_places.items():
+    print(key.title() + " loves to visit " + value.title())
+
+# Make dictionary called cities, create another dictionary w/ info about each
