446732c3d338ce99c03511efa9da938555dc72aa
Anna Pawl <annampawl@gmail.com>
2018-11-23 15:00:02 -0800

    chapter 7 exercises

---
+++ Chapter7_User_Input_While_Loops/chapter7_exercises.py
@@ -0,0 +1,39 @@
+# RENTAL CAR PROGRAM
+# Ask user what kind of car they want with print message
+
+#RESTAURANT SEATING
+# Ask user how many people coming. If answer is more than 8, print message that they'll have to wait for table. Otherwise, notify that table is ready
+
+# MULTIPLES OF TEN
+# Ask usr for a number, report whether it's a multiple of TEN
+
+# ICECREAM TOPPINGS
+# Loop that prompts user to enter toppings until they quit program. As they enter each topping, print message that you'll add it to their icecream
+
+#MOVIE TICKETS
+# Theater charges dif prices depending on age
+# Under 3- free, under 12-$10, under 20- $15
+# Write loop to ask users age and then tell them ticket cost
+
+# THREE EXITS
+# Build off Icecream and..
+# Use conditional test in while loop to stop Loop
+# Use active variable to control how long loop runs
+# Use break statement to exit loop when user quits
+
+# INFINITY
+# Write a loop that never ends!
+
+# DELI ORDERS
+# Make list of orders, and empty list called
+# Loop through orders and print statement for each
+# As each order is name, mode it to finished orders list
+# Final print statement when all orders complete
+
+# NO PASTRAMI
+# Using exmaple above, note that deli ran out of pastrami
+# Use while loop to remove all occurances of pastrami from orders
+
+# DREAM VACATION
+# Poll users about their dream vacation
+# Include code block  that prints poll results 
